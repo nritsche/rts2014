02a9a538211429da830bc33df87ff62474e3726d
Yulong Bai <yulongb@stud.ntnu.no>
2014-08-28 14:42:09 +0200

    insert

--- ex1/linked_list/linked_list.c
+++ ex1/linked_list/linked_list.c
@@ -26,7 +26,16 @@ void list_delete(list_t list)
 
 void list_insert(list_t list, int index, int data)
 {
-
+	if(!list){
+			perror("the argument invalid");
+			return;
+		}
+	struct node * current = list->head;
+	for(int i = 0; i<= index; i++){
+		current = current->next;
+	}
+	current->data = data;
+	current =  NULL;/* tangle pointer */
 }
 
 void list_append(list_t list, int data)
@@ -41,15 +50,7 @@ void list_print(list_t list)
 
 long list_sum(list_t list)
 {
-	int sum = 0;
-	struct node cur = list->head;
 
-	while (cur->next != NULL) {
-		sum += cur->data;
-		cur = cur->next;
-	}
-
-	return sum;
 }
 
 int list_get(list_t list, int index)
@@ -59,8 +60,6 @@ int list_get(list_t list, int index)
 	while (i != index) {
 		cur = cur->next;
 		i++;
-		if (cur->next == NULL && i != index)
-			return 0;
 	}
 	return cur->data;
 }
@@ -73,24 +72,12 @@ int list_extract(list_t list, int index)
 	while (i != index) {
 		cur = cur->next;
 		i++;
-		if (cur->next == NULL && i != index)
-			return 0;
 	}
 	ret = cur->data;
 
 	//remove node
-	if (cur->prev == NULL) { //remove head
-		list->head = cur->next;
-		cur->next->prev = NULL;
-	}
-	else if (cur->next == NULL) { //remove tail
-		list->tail = cur->prev;
-		cur->prev->next = NULL;
-	}
-	else {
-		cur->prev->next = cur->next;
-		cur->next->prev = cur->prev;
-	}
+	cur->prev->next = cur->next;
+	cur->next->prev = cur->prev;
 	free(cur);
 
 	return ret;
